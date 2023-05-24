984252cb67fbef7108dfd51ff7d76b3cb22c39dd
cstoicescu <stoicescu.catalinn97@gmail.com>
2023-05-25 00:35:27 +0300

    add_to_env bash only

--- add_to_env.sh
+++ add_to_env.sh
@@ -1,43 +1,29 @@
 #!/bin/bash
 
-# Define the path to the msg-opener files
-MSG_OPENER_PATH="C:/Users/Catalin/Desktop/GitHub/MsgOpener"
+# Get the directory path where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Define the msgview alias based on the operating system
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    MSGVIEW_ALIAS="msgviewer-linux"
+# Determine the operating system
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+    # Windows
+    FILE_PATH="$SCRIPT_DIR/msgviewer-win.exe"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    FILE_PATH="$SCRIPT_DIR/msgviewer-linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    MSGVIEW_ALIAS="msgviewer-macos"
-elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
-    MSGVIEW_ALIAS="msgviewer-win.exe"
+    # macOS
+    FILE_PATH="$SCRIPT_DIR/msgviewer-macos"
 else
     echo "Unsupported operating system."
     exit 1
 fi
 
-# Normalize the path for Windows if running in Git Bash
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
-    MSG_OPENER_PATH=$(cygpath -m "$MSG_OPENER_PATH")
-fi
+# Add the file path to the PATH in .bash_profile
+echo "export PATH=\"\$PATH:$FILE_PATH\"" >> "$HOME/.bash_profile"
 
-# Check if the entries already exist in the .bash_profile
-if grep -Fxq "# Add 'msgviewer' files to PATH" "$HOME/.bash_profile" && grep -Fxq "export PATH=\"\$PATH:$MSG_OPENER_PATH\"" "$HOME/.bash_profile" && grep -Fxq "alias msgview=\"$MSGVIEW_ALIAS\"" "$HOME/.bash_profile"; then
-    echo "The entries already exist in .bash_profile."
-else
-    # Append the entries to the .bash_profile
-    printf "\n# Add 'msgviewer' files to PATH\n" >> "$HOME/.bash_profile"
-    printf "export PATH=\"\$PATH:$MSG_OPENER_PATH\"\n" >> "$HOME/.bash_profile"
-    printf "alias msgview=\"$MSGVIEW_ALIAS\"\n" >> "$HOME/.bash_profile"
-    echo "Updated .bash_profile with the necessary entries."
-fi
+# Add an alias for convenience
+echo "alias msgview=\"$FILE_PATH\"" >> "$HOME/.bash_profile"
 
-# Check if the msgviewer path is already added to the PATH for CMD
-if echo "%PATH%" | grep -q "$MSG_OPENER_PATH"; then
-    echo "The msgviewer path is already added to the PATH for CMD."
-else
-    # Add the msgviewer path to the PATH for CMD
-    setx PATH "%PATH%;$MSG_OPENER_PATH"
-    echo "Updated PATH environment variable for CMD."
-fi
-
-echo "Please restart Git Bash and CMD to apply the changes."
+# Print a success message
+echo "The file '$FILE_PATH' has been added to the PATH in .bash_profile."
+echo "An alias 'msgview' has been added as well."
