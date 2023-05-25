7fa85a4677376585b410d752ba5725d4ffe2dad8
Stoicescu Catalin <catalin-costin.stoicescu@ing.com>
2023-05-25 13:13:41 +0300

    move style to .html

--- add_to_env.sh
+++ add_to_env.sh
@@ -18,12 +18,22 @@ else
     exit 1
 fi
 
-# Add the file path to the PATH in .bash_profile
-echo "export PATH=\"\$PATH:$FILE_PATH\"" >> "$HOME/.bash_profile"
+# Add the file path and alias to .bash_profile
+if [ -f "$HOME/.bash_profile" ]; then
+    echo "export PATH=\"\$PATH:$FILE_PATH\"" >> "$HOME/.bash_profile"
+    echo "alias msgview=\"$FILE_PATH\"" >> "$HOME/.bash_profile"
+    echo "The file '$FILE_PATH' has been added to the PATH in .bash_profile."
+    echo "An alias 'msgview' has been added as well."
+else
+    echo "The .bash_profile file does not exist. Please manually add the file path and alias to your shell configuration file."
+fi
 
-# Add an alias for convenience
-echo "alias msgview=\"$FILE_PATH\"" >> "$HOME/.bash_profile"
-
-# Print a success message
-echo "The file '$FILE_PATH' has been added to the PATH in .bash_profile."
-echo "An alias 'msgview' has been added as well."
+# Add the file path and alias to .zshrc
+if [ -f "$HOME/.zshrc" ]; then
+    echo "export PATH=\"\$PATH:$FILE_PATH\"" >> "$HOME/.zshrc"
+    echo "alias msgview=\"$FILE_PATH\"" >> "$HOME/.zshrc"
+    echo "The file '$FILE_PATH' has been added to the PATH in .zshrc."
+    echo "An alias 'msgview' has been added as well."
+else
+    echo "The .zshrc file does not exist. Please manually add the file path and alias to your shell configuration file."
+fi
